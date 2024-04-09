48bfb6fd2c028beb464b7d09b96e04d41931a286
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-04-09 10:55:48 +0700

    fix: update unit test for UserProfile

--- src/components/UserProfile/__test__/index.test.tsx
+++ src/components/UserProfile/__test__/index.test.tsx
@@ -1,31 +1,19 @@
 // Libs
-import { fireEvent, screen, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 // Components
 import UserProfile from '..';
 
-const mockProps = {
-  onLogout: jest.fn(),
-};
-
 describe('UserProfile components', () => {
   test('should render UserProfile successfully', () => {
-    const { container } = render(<UserProfile {...mockProps} />);
+    const { container } = render(<UserProfile />);
 
     expect(container).toBeInTheDocument();
   });
 
   test('should match snapshot for UserProfile', () => {
-    const { container } = render(<UserProfile {...mockProps} />);
+    const { container } = render(<UserProfile />);
 
     expect(container).toMatchSnapshot();
-  });
-
-  test('should call onLogout when click the logout button', () => {
-    render(<UserProfile {...mockProps} />);
-
-    fireEvent.click(screen.getByText('Logout'));
-
-    expect(mockProps.onLogout).toHaveBeenCalled();
   });
 });
