dba6eccdff6d3745be009b627b90e51c69eda431
Ngoc Ngo <131107648+ngocngo-agilityio@users.noreply.github.com>
2024-04-09 20:00:57 +0700

    fix: convention

--- src/app/cart/CartItems.tsx
+++ src/app/cart/CartItems.tsx
@@ -82,7 +82,12 @@ const CartItems = (): JSX.Element => {
         </Flex>
       </Box>
 
-      <Button variant="checkoutBtn" fontSize="20px" w="262px" h="58px" onClick={handleCheckout}>
+      <Button
+        variant="checkoutBtn"
+        fontSize="20px"
+        w="262px"
+        h="58px"
+        onClick={handleCheckout}>
         Proceed to Checkout
       </Button>
     </>
