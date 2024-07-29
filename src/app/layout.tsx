9c2df60f562a0f861b1e04092ff678c5b1070b4d
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-07-29 23:09:21 +0700

    fix: add favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
   title: 'E-commerce Fashion',
   description:
     'Discover the latest trends and shop your favorite styles on our innovative e-commerce platform designed for fashion enthusiasts.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -27,9 +30,9 @@ export default function RootLayout({
       <body>
         <ChakraUIProvider>
           <MainLayout>
-            {/* 
-            This Box component ensures that the content area takes up at least 
-            the height of the viewport minus the heights of the header and footer 
+            {/*
+            This Box component ensures that the content area takes up at least
+            the height of the viewport minus the heights of the header and footer
           */}
             <Box
               minHeight={`calc(100vh - ${HEADER_HEIGHT} - ${FOOTER_HEIGHT})`}
