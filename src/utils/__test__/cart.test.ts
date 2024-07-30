62460dfa7de0881c0e20fc9af13e9f4eeacbcc40
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-07-30 21:30:05 +0700

    fix: update unit test for cart utils

--- src/utils/__test__/cart.test.ts
+++ src/utils/__test__/cart.test.ts
@@ -4,6 +4,9 @@ import { MOCK_CART_ITEMS } from '@mocks';
 // Utils
 import { calculateTotalPrice } from '../cart';
 
+// Types
+import { CartItem } from '@types';
+
 describe('calculateTotalPrice currency', () => {
   test('returns 0 when cartItems is empty', () => {
     const cartItems = undefined;
@@ -19,5 +22,13 @@ describe('calculateTotalPrice currency', () => {
     const result = calculateTotalPrice(cartItems);
 
     expect(result).toEqual(138);
+  });
+
+  test('returns formatted currency correct if cart item is null', () => {
+    const cartItems = [null as unknown as CartItem];
+
+    const result = calculateTotalPrice(cartItems);
+
+    expect(result).toEqual(0);
   });
 });
