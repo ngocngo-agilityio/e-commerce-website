512c2263ac91ec4b76aa273ac30c6e1902a8e1e9
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-07-30 11:32:28 +0700

    fix: update unit test for Sort component

---
+++ src/mocks/sort.ts
@@ -0,0 +1,7 @@
+// Types
+import { SortOption } from '@types';
+
+export const MOCK_SORT_OPTIONS: SortOption[] = [
+  { value: '1', label: 'Increment by name' },
+  { value: '2', label: 'Decrement by name' },
+];
