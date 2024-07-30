a45aed9cb6f137cb3cf43aa6b4e5e91e37b49a53
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-07-30 10:53:06 +0700

    fix: update unit test for Skeletons component

--- src/components/Skeleton/SkeletonProductCategories/__test__/index.test.tsx
+++ src/components/Skeleton/SkeletonProductCategories/__test__/index.test.tsx
@@ -5,12 +5,6 @@ import { render } from '@testing-library/react';
 import SkeletonProductCategories from '..';
 
 describe('SkeletonProductCategories component', () => {
-  test('should render SkeletonProductCategories successfully', () => {
-    const { container } = render(<SkeletonProductCategories />);
-
-    expect(container).toBeInTheDocument();
-  });
-
   test('should match snapshot for SkeletonProductCategories', () => {
     const { container } = render(<SkeletonProductCategories />);
 
--- src/components/Skeleton/SkeletonProductList/__test__/index.test.tsx
+++ src/components/Skeleton/SkeletonProductList/__test__/index.test.tsx
@@ -5,12 +5,6 @@ import { render } from '@testing-library/react';
 import SkeletonProductList from '..';
 
 describe('SkeletonProductList component', () => {
-  test('should render SkeletonProductList successfully', () => {
-    const { container } = render(<SkeletonProductList />);
-
-    expect(container).toBeInTheDocument();
-  });
-
   test('should match snapshot for SkeletonProductList', () => {
     const { container } = render(<SkeletonProductList />);
 
