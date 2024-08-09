b1c0307b6ced19033330a44ef8d67c085d861c60
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-08-09 15:56:06 +0700

    fix: refactor for unit tests of SignInForm and SignUpForm component

--- src/components/LoginForm/__test__/index.test.tsx
+++ src/components/LoginForm/__test__/index.test.tsx
@@ -18,6 +18,31 @@ import LoginForm from '../index';
 describe('LoginForm', () => {
   const mockOnSignIn = jest.fn();
 
+  const submitForm = (email: string, password: string) => {
+    const EMAIL_FIELD = screen.getByPlaceholderText('Your email');
+    const PASSWORD_FIELD = screen.getByPlaceholderText('Enter password');
+    const TOGGLE_PASSWORD_ICON = screen.getByTestId('view-icon');
+    const SUBMIT_BTN = screen.getByText('Sign in');
+
+    act(() => {
+      // Change email
+      fireEvent.change(EMAIL_FIELD, {
+        target: { value: email },
+      });
+      fireEvent.blur(EMAIL_FIELD);
+
+      // Change password
+      fireEvent.change(PASSWORD_FIELD, {
+        target: { value: password },
+      });
+      fireEvent.blur(PASSWORD_FIELD);
+
+      fireEvent.click(TOGGLE_PASSWORD_ICON);
+
+      fireEvent.click(SUBMIT_BTN);
+    });
+  };
+
   test('should match with snapshot', () => {
     const { container } = render(<LoginForm onSignIn={mockOnSignIn} />);
 
@@ -27,25 +52,8 @@ describe('LoginForm', () => {
   test('should call onSignIn when submit the valid form', () => {
     render(<LoginForm onSignIn={mockOnSignIn} />);
 
-    const EMAIL_FIELD = screen.getByPlaceholderText('Your email');
-    const PASSWORD_FIELD = screen.getByPlaceholderText('Enter password');
-    const TOGGLE_PASSWORD_ICON = screen.getByTestId('view-icon');
-    const SUBMIT_BTN = screen.getByText('Sign in');
-
     // Change form
-    act(() => {
-      fireEvent.change(EMAIL_FIELD, {
-        target: { value: MOCK_SIGN_IN_PAYLOAD.email },
-      });
-
-      fireEvent.change(PASSWORD_FIELD, {
-        target: { value: MOCK_SIGN_IN_PAYLOAD.password },
-      });
-
-      fireEvent.click(TOGGLE_PASSWORD_ICON);
-
-      fireEvent.click(SUBMIT_BTN);
-    });
+    submitForm(MOCK_SIGN_IN_PAYLOAD.email, MOCK_SIGN_IN_PAYLOAD.password);
 
     waitFor(() => {
       expect(mockOnSignIn).toHaveBeenCalledWith(MOCK_SIGN_IN_PAYLOAD);
@@ -55,23 +63,8 @@ describe('LoginForm', () => {
   test('should show errors when blur with the invalid field', async () => {
     render(<LoginForm onSignIn={mockOnSignIn} />);
 
-    const EMAIL_FIELD = screen.getByPlaceholderText('Your email');
-    const PASSWORD_FIELD = screen.getByPlaceholderText('Enter password');
-
     // Change form
-    act(() => {
-      fireEvent.change(EMAIL_FIELD, {
-        target: { value: 'a' },
-      });
-
-      fireEvent.blur(EMAIL_FIELD);
-
-      fireEvent.change(PASSWORD_FIELD, {
-        target: { value: 'a' },
-      });
-
-      fireEvent.blur(PASSWORD_FIELD);
-    });
+    submitForm('a', 'a');
 
     await waitFor(() => {
       expect(ERROR_MESSAGES.FIELD_INVALID('Email')).toBeTruthy();
--- src/components/SignUpForm/__test__/index.test.tsx
+++ src/components/SignUpForm/__test__/index.test.tsx
@@ -13,20 +13,18 @@ import { MOCK_SIGN_UP_PAYLOAD } from '@mocks';
 import { ERROR_MESSAGES } from '@constants';
 
 // Components
-import SignUpForm from '../index';
+import SignUpForm, { ISignUpForm } from '../index';
 
 describe('SignUpForm', () => {
   const mockOnSignUp = jest.fn();
 
-  test('should match with snapshot', () => {
-    const { container } = render(<SignUpForm onSignUp={mockOnSignUp} />);
-
-    expect(container).toMatchSnapshot();
-  });
-
-  test('should call onSignUp when submit the valid form', () => {
-    render(<SignUpForm onSignUp={mockOnSignUp} />);
-
+  const submitForm = ({
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+  }: ISignUpForm) => {
     const FIRST_NAME = screen.getByPlaceholderText('First name');
     const LAST_NAME = screen.getByPlaceholderText('Last name');
     const EMAIL_FIELD = screen.getByPlaceholderText('Email');
@@ -39,36 +37,56 @@ describe('SignUpForm', () => {
     );
     const SUBMIT_BTN = screen.getByText('Sign up');
 
-    // Change form
     act(() => {
       // Change First name field
       fireEvent.change(FIRST_NAME, {
-        target: { value: MOCK_SIGN_UP_PAYLOAD.firstName },
+        target: { value: firstName },
       });
+      fireEvent.blur(FIRST_NAME);
 
       // Change Last name field
       fireEvent.change(LAST_NAME, {
-        target: { value: MOCK_SIGN_UP_PAYLOAD.lastName },
+        target: { value: lastName },
       });
+      fireEvent.blur(LAST_NAME);
 
       // Change Email field
       fireEvent.change(EMAIL_FIELD, {
-        target: { value: MOCK_SIGN_UP_PAYLOAD.email },
+        target: { value: email },
       });
+      fireEvent.blur(EMAIL_FIELD);
 
       // Change Password field
       fireEvent.change(PASSWORD_FIELD, {
-        target: { value: MOCK_SIGN_UP_PAYLOAD.password },
+        target: { value: password },
       });
+      fireEvent.blur(PASSWORD_FIELD);
       fireEvent.click(TOGGLE_PASSWORD_ICON);
 
       // Change Confirm Password field
       fireEvent.change(CONFIRM_PASSWORD_FIELD, {
-        target: { value: MOCK_SIGN_UP_PAYLOAD.password },
+        target: { value: confirmPassword },
       });
+      fireEvent.blur(CONFIRM_PASSWORD_FIELD);
       fireEvent.click(TOGGLE_CONFIRM_PASSWORD_ICON);
 
       fireEvent.click(SUBMIT_BTN);
+    });
+  };
+
+  test('should match with snapshot', () => {
+    const { container } = render(<SignUpForm onSignUp={mockOnSignUp} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should call onSignUp when submit the valid form', () => {
+    render(<SignUpForm onSignUp={mockOnSignUp} />);
+
+    // Change form
+    submitForm({
+      ...MOCK_SIGN_UP_PAYLOAD,
+      confirmPassword: MOCK_SIGN_UP_PAYLOAD.password,
     });
 
     waitFor(() => {
@@ -79,44 +97,13 @@ describe('SignUpForm', () => {
   test('should show errors when blur with the invalid field', async () => {
     render(<SignUpForm onSignUp={mockOnSignUp} />);
 
-    const FIRST_NAME = screen.getByPlaceholderText('First name');
-    const LAST_NAME = screen.getByPlaceholderText('Last name');
-    const EMAIL_FIELD = screen.getByPlaceholderText('Email');
-    const PASSWORD_FIELD = screen.getByPlaceholderText('Password');
-    const CONFIRM_PASSWORD_FIELD =
-      screen.getByPlaceholderText('Confirm password');
-
     // Change form
-    act(() => {
-      // Change First name field
-      fireEvent.change(FIRST_NAME, {
-        target: { value: '' },
-      });
-      fireEvent.blur(FIRST_NAME);
-
-      // Change Last name field
-      fireEvent.change(LAST_NAME, {
-        target: { value: '' },
-      });
-      fireEvent.blur(LAST_NAME);
-
-      // Change Email field
-      fireEvent.change(EMAIL_FIELD, {
-        target: { value: 'user1' },
-      });
-      fireEvent.blur(EMAIL_FIELD);
-
-      // Change Password field
-      fireEvent.change(PASSWORD_FIELD, {
-        target: { value: 'Abcd' },
-      });
-      fireEvent.blur(PASSWORD_FIELD);
-
-      // Change Confirm Password field
-      fireEvent.change(CONFIRM_PASSWORD_FIELD, {
-        target: { value: 'Abc' },
-      });
-      fireEvent.blur(CONFIRM_PASSWORD_FIELD);
+    submitForm({
+      firstName: '',
+      lastName: '',
+      email: 'user1',
+      password: 'Abcd',
+      confirmPassword: 'Abc',
     });
 
     await waitFor(() => {
