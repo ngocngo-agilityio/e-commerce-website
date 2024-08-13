e390ec3f11ec53d5ba414326010a11e00d738f57
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-08-13 10:31:55 +0700

    fix: write unit test for LoginForm

---
+++ src/components/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+// Components
+import LoginForm from '.';
+
+const meta = {
+  title: 'LoginForm',
+  component: LoginForm,
+  parameters: {
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    isSubmitting: false,
+    onSignIn: () => {},
+  },
+};
